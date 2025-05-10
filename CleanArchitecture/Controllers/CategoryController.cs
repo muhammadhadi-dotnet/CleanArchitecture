46e0932db45e4c694d40ba2f83ac9103d7d343dd
muhammadhadi-dotnet <hadikhandev364@gmail.com>
2025-05-11 04:18:55 +0500

    category curd in controller

---
+++ CleanArchitecture/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using CleanArchitecture.Domain.Entity;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly MyDbContext _context;
+        public CategoryController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categores = await _context.Categories.ToListAsync();
+
+            return View(categores);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category=await _context.Categories.FindAsync(id);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Category category)
+        {
+
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
